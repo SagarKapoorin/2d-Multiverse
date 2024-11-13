d04f60b04b857bd61e583cdfea8cfa7e275942e1
Your GitHub Username <sagarbadal70@gmail.com>
2024-11-13 17:34:49 +0530

    Added Middleware-checking token and admin access

--- 2D-Multiverse/apps/Http/src/middlewares/admin.ts
+++ 2D-Multiverse/apps/Http/src/middlewares/admin.ts
@@ -1,10 +1,11 @@
 
 import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
-
+//three things->adding user id to rquest , checking token , checking admin acess
 export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const header = req.headers["authorization"];
     const token = header?.split(" ")[1];
+    //['Bearer','Token']
     if (!token) {
         res.status(403).json({message: "Unauthorized"})
         return
---
+++ 2D-Multiverse/apps/Http/src/middlewares/user.ts
@@ -0,0 +1,25 @@
+
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const token = header?.split(" ")[1];
+    if (!token) {
+        res.status(403).json({message: "Unauthorized"})
+        return
+    }
+    try {
+        const secret=process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error("JWT_SECRET is not defined in environment variables");
+        }
+        const decoded = jwt.verify(token, secret) as { role: string, userId: string };
+      
+        req.userId = decoded.userId
+        next()
+    } catch(e) {
+        res.status(401).json({message: "Unauthorized"})
+        return
+    }
+}
